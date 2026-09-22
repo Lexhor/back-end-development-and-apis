aa534b6099dba5f1b5bd063e1e24911b820d864d
Lexhor <64706048+Lexhor@users.noreply.github.com>
2026-09-22 18:54:13 +0000

    feat: complete timestamp microservice challenge

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -13,6 +13,34 @@ app.get("/", (_req, res) => {
 
 // Do not change code above this line
 
+app.get("/api", (_req, res) => {
+  const now = new Date();
+  res.json({
+    unix: now.getTime(),
+    utc: now.toUTCString()
+  });
+});
+
+app.get("/api/:date", (req, res) => {
+  const dateParam = req.params.date;
+  let date;
+
+  if (/^\d+$/.test(dateParam)) {
+    date = new Date(parseInt(dateParam));
+  } else {
+    date = new Date(dateParam);
+  }
+
+  if (isNaN(date.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  res.json({
+    unix: date.getTime(),
+    utc: date.toUTCString()
+  });
+});
+
 // Do not change code below this line
 
 const PORT = 8000;
